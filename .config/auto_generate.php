c6d3a5f3d4f1e2b7bcf058b42bf307823d8e4028
한빛가람(ハンビッガラム) <webmaster@hanb.jp>
2023-01-29 02:41:55 +0900

    AUTO COMMIT

--- .config/auto_generate.php
+++ .config/auto_generate.php
@@ -12,13 +12,16 @@ CF;
 
 for($i=0, $content=explode(PHP_EOL, GET('https://www.cloudflare.com/ips-v4')), $cnt=count($content); $i<$cnt; $i++){
     $CF = "allow {$content[$i]};".PHP_EOL.$CF;
+    $CF = "set_real_ip_from {$content[$i]};".PHP_EOL.$CF;
 }
 for($i=0, $content=explode(PHP_EOL, GET('https://www.cloudflare.com/ips-v6')), $cnt=count($content); $i<$cnt; $i++){
     $CF = "allow {$content[$i]};".PHP_EOL.$CF;
+    $CF = "set_real_ip_from {$content[$i]};".PHP_EOL.$CF;
 }
 
 // 캐리지 리턴 제거, 개행만 남김
 $CF = str_replace("\r", "","# Last Update : ".date('Y-m-d H:i:s').'+09:00'.PHP_EOL.$CF);
+$CF .= PHP_EOL."real_ip_header CF-Connecting-IP;";
 file_put_contents(_BASE.'/nginx/cloudflare.conf', $CF);
 ##################################################################################
 
